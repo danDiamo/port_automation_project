9c30288c1269e66f98c7332101f7a91573d78e34
danDiamo <78231894+danDiamo@users.noreply.github.com>
2026-01-06 21:24:08 -0600

    Removed placeholder files from Git

--- collection.py
+++
@@ -1,2 +0,0 @@
-"""This file will hold a 'Collection' Python class, modeling a collection of
-digital music scores"""
--- soundslice_utils.py
+++
@@ -1,6 +0,0 @@
-"""This file will hold utility functions for interfacing with the SoundSlice
-Python API
-
-Note: some of this functionality may eventually be superseded by direct API
-calls"""
-
