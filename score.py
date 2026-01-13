59812f197c12fcb95f7036c9c1d3e8cb5a28f1a5
danDiamo <78231894+danDiamo@users.noreply.github.com>
2026-01-13 17:47:55 -0600

    Added methods to extract all data from MusicXML, along with happy path tests.

--- score.py
+++ score.py
@@ -1,11 +1,16 @@
 """This file will hold a 'Score' Python class, modeling a single digital music
 score"""
 
+# TODO: error handling
+
 import music21
 import os
 import pandas as pd
 
-from music21 import key
+from collections import Counter
+from music21 import analysis, bar, key, meter, note, chord
+from pandas.core.config_init import max_cols
+
 
 class Score:
 
@@ -19,7 +24,11 @@ class Score:
     Attributes:
 
     score_path -- path to a MusicXML music score file.
+    content -- music21 Stream object representing the musical content.
+    incipit -- music21 Stream object representing the 4-bar incipit.
+    key -- music21 KeySignature object representing the key signature.
     metadata_path -- path to a csv file containing metadata for the score.
+    
     """
 
     METADATA_FIELDS = [
@@ -42,7 +51,9 @@ class Score:
         self.score_path = score_path
         # ensure that score_path points to a MusicXML file
         self._validate_score_file()
-        self.score_content = None
+        self.content = None
+        self.incipit = None
+        self.key = None     # TODO: make a property
         self.metadata_path = None
         pass
 
@@ -63,30 +74,163 @@ class Score:
                 f"Unsupported file type: {self.score_path.suffix}"
             )
 
-    def read_score_content_to_music21_stream(self):
+    def read_content_to_music21_stream(self):
         """Reads content from MusicXML file into a music21 Stream object."""
-        self.score_content = music21.converter.parse(self.score_path)
+        self.content = music21.converter.parse(self.score_path)
 
     def detect_key_signature(self):
-        # detect key via Music21-s built-in Krumhansl-Schmuckler algorithm
-        detected = self.score_content.analyze("key")
-        return detected
+        """Detects key via Music21-s built-in Krumhansl-Schmuckler algorithm"""
 
-    def read_key_signature_from_score(self):
-        # get any KeySignature objects provided within the score
-        # TODO: Compare & count keys returned to come up with a single output.
+        # TODO: Discuss this implementation with ITMA
+
+        # Detect key of each 4-bar 'half phrase' in the score
+        key_analysis = analysis.floatingKey.KeyAnalyzer(self.content)
+        key_analysis.windowSize = 8
+        detected_keys = key_analysis.run()
+
+        # Check that at least one Key object was actually returned
+        if detected_keys is None:
+            raise ValueError("No key signature detected.")
+
+        # count and rank key signature occurrences
+        ranked_key_sigs = Counter(detected_keys)
+        max_count = ranked_key_sigs.most_common(1)[0][1]
+        # identify the most frequently occurring key signature(s)
+        top_ranked = [
+            key for key, count in ranked_key_sigs.items() if count == max_count
+        ]
+        # Return the first occurring of the top-ranked key signature(s)
+        self.key = top_ranked[0]
+        return top_ranked[0]
+
+    def read_encoded_key_signature_from_score(self):
+        """Get any KeySignature objects provided within the score"""
+
+        content = self.content.recurse()
+        key_sigs = [ks for ks in content.getElementsByClass(key.KeySignature)]
+        # handle cases were no key signatures were provided
+        if not key_sigs:
+            raise ValueError(
+                f"This score does not contain any key signature objects. ")
+        # Return only the first key signature object encoded in the score
+        self.key = key_sigs[0]
+        return key_sigs[0]
 
     def extract_tonic_from_key_signature(self):
-        pass
+
+        """
+        Extracts the tonic pitch name from the key signature returned by
+        the two
+        methods above.
+        """
+
+        key = self.key
+        if key is None:
+            raise ValueError("Cannot extract tonic as this score does not "
+                             "contain any signature information. Please "
+                             "either detect and/or read key signature, "
+                             "then retry.")
+        # read tonic and return in human-readable format
+        return key.tonic.name
+
 
     def extract_mode_from_key_signature(self):
-        pass
+        """Extracts the mode from the score's key signature property"""
+
+        key = self.key
+        if key is None:
+            raise ValueError("Cannot extract mode as this score does not "
+                             "contain any signature information. Please "
+                             "either detect and/or read key signature, "
+                             "then retry.")
+        return key.mode
 
     def extract_time_signature(self):
-        pass
+        """Extracts the time signature from the score"""
+
+        # TODO: Discuss implementation with ITMA -- only takes first time sig
+
+        all_time_signatures = self.content[meter.TimeSignature]
+        # Make sure at least one time signature was found
+        if not all_time_signatures:
+            raise ValueError(
+                "This score does not contain any time signature objects."
+            )
+
+        # Read the first time signature and return in human-readable format
+        time_signature = all_time_signatures[0]
+        return time_signature.ratioString
+
+    def extract_incipit(self):
+        """Extracts a 4-bar incipit from the score"""
+
+        # TODO: Discuss with ITMA: will score always be single-line melody?
+        #  finish functionality here according to answer!
+
+        content = self.content
+        #  check score is not empty
+        if not content:
+            raise ValueError(
+                "Cannot extract incipit: either this score is empty or it is "
+                "not loading correctly."
+            )
+        # return first 4 bars
+        incipit = content.measures(1, 4)
+        self.incipit = incipit
+        return incipit
 
     def create_breathnach_codes(self):
-        pass
+
+        """
+        Creates Breathnach code. These codes are diatonic scale degree
+        sequences representing rhythmically-emphasised notes in the incipit.
+        """
+
+        # TODO:
+        #  test on other time signatures
+        #  check with ITMA if we'll need to handle multi-part scores
+        #  test handling of rests and chords
+
+        incipit = self.incipit
+        key_sig = incipit.analyze('key')
+        diatonic_scale = key_sig.getScale()
+
+        # Remove expressions and articulation
+        for n in incipit.recurse().notes:
+            n.expressions = []
+
+        accented_notes = []
+        # filter to retain accented notes only
+        # Extract scale degrees for accented notes and store in list
+        # Note: use of flatten vs recurse here may change as testing proceeds
+        for n in incipit.flatten().notes:
+                if n.isNote and n.beat == int(n.beat):
+                    scale_degree = diatonic_scale.getScaleDegreeFromPitch(
+                        n.pitch)
+                    accented_notes.append(scale_degree)
+
+        return accented_notes
+    
+    def count_number_of_parts(self):
+        
+        """
+        Counts the number of parts in the tune by applying a simple 
+        heuristic: double barlines are taken as indicators of part 
+        structure.
+        
+        This is not foolproof and a manual pass may be required after 
+        running this function.
+        """
+        score = self.content
+        barlines = score.recurse().getElementsByClass(bar.Barline)
+
+        # derive part structure by identifying double & final barlines
+        part_structure = [
+            b for b in barlines if b.type == 'double' or b.type == 'final'
+                           ]
+        # count parts (total = number of markers + 1)
+        num_parts = len(part_structure)
+        return num_parts
 
     def convert_score_to_midi(self):
         pass
@@ -98,9 +242,6 @@ class Score:
         pass
 
     def copy_musicxml_file_to_aws(self):
-        pass
-
-    def extract_incipit_from_score(self):
         pass
 
     def convert_incipit_to_midi(self):
@@ -126,7 +267,7 @@ class Score:
         # private helper method acting on self.metadata DataFrame
         pass
 
-    def extract_metadat(self):
+    def extract_metadata(self):
         # TODO: call _extract_metadata_field for fields listed in
         #  METADATA_FIELDS class constant
         pass
--- tests/test_score.py
+++ tests/test_score.py
@@ -22,19 +22,117 @@ def default_score():
     """Provides a standard User instance for testing."""
     return Score(happy_testfile)
 
-def test_score_path_(default_score):
+def test_score_path(default_score):
     # Test init of Score object using happy_testfile
     assert isinstance(default_score, Score)
 
-def test_loading_score_content_(default_score):
-    default_score.read_score_content_to_music21_stream()
-    assert isinstance(default_score.score_content, stream.Stream)
+def test_loading_content(default_score):
+    """Test reading score content to Music21 stream"""
+    # Load test class instance
+    default_score.read_content_to_music21_stream()
+    assert isinstance(default_score.content, stream.Stream)
 
-def test_key_signature_detection_():
+def test_key_signature_detection():
+    """Test our usage of Music21's algorithmic key signature detection"""
     default_score = Score(happy_testfile)
-    default_score.read_score_content_to_music21_stream()
-    key_sig = default_score.detect_key_signature()
-    assert isinstance(key_sig, music21.key.Key)
+    default_score.read_content_to_music21_stream()
+
+    detected_key_sig = None
+    # read key signature(s) & handle error if none are present
+    try:
+        detected_key_sig = default_score.detect_key_signature()
+    except ValueError as e:
+        print(f"Error: {e}")
+    # check type of encoded key signature
+    assert isinstance(detected_key_sig, music21.key.Key)
+
+def test_read_key_signature_from_score():
+    """Test reading key signature(s) directly from MusicXML file"""
+    default_score = Score(happy_testfile)
+    default_score.read_content_to_music21_stream()
+
+    encoded_key_sig = None
+    # read key signature(s) & handle error if none are present
+    try:
+        encoded_key_sig = default_score.read_encoded_key_signature_from_score()
+    except ValueError as e:
+        print(f"Error: {e}")
+    # check type of encoded key signature
+    assert isinstance(encoded_key_sig, music21.key.Key)
+
+def test_extract_tonic_from_key_signature():
+    """Test extracting tonic from key signature"""
+
+    default_score = Score(happy_testfile)
+    default_score.read_content_to_music21_stream()
+    default_score.detect_key_signature()
+    tonic = default_score.extract_tonic_from_key_signature()
+    assert isinstance(tonic, str)
+
+def test_extract_mode_from_key_signature():
+    """Test extracting mode from key signature"""
+
+    default_score = Score(happy_testfile)
+    default_score.read_content_to_music21_stream()
+    default_score.detect_key_signature()
+    mode = default_score.extract_mode_from_key_signature()
+    assert isinstance(mode, str)
+
+def test_extract_time_signature():
+    """Test extracting time signature from score"""
+
+    default_score = Score(happy_testfile)
+    default_score.read_content_to_music21_stream()
+
+    time_sig = None
+    try:
+        time_sig = default_score.extract_time_signature()
+    except ValueError as e:
+        print(f"Error: {e}")
+
+    time_sig = default_score.extract_time_signature()
+    time_sig_elements = time_sig.split('/')
+    assert (len(time_sig_elements) == 2
+            and time_sig_elements[0].isdigit()
+            and time_sig_elements[1].isdigit())
+
+def test_extract_incipit():
+    """Test extracting incipit from score"""
+
+    default_score = Score(happy_testfile)
+    default_score.read_content_to_music21_stream()
+
+    incipit = default_score.extract_incipit()
+    melody = incipit.getElementsByClass('Part')[0]
+    measures = melody.getElementsByClass('Measure')
+    assert isinstance(incipit, music21.stream.Stream) and len(measures) == 4
+
+def test_create_breathnach_codes():
+    """Test creating Breathnach codes"""
+
+    default_score = Score(happy_testfile)
+    default_score.read_content_to_music21_stream()
+    default_score.extract_incipit()
+
+    test_codes = default_score.create_breathnach_codes()
+    assert all(isinstance(x, int) and 1 <= x <= 7 for x in test_codes)
+
+def test_count_number_of_parts():
+    """Test counting the number of parts in a tune"""
+
+    default_score = Score(happy_testfile)
+    default_score.read_content_to_music21_stream()
+
+    assert default_score.count_number_of_parts() == 2
+    
+
+
+
+
+
+
+
+
 
 
 
